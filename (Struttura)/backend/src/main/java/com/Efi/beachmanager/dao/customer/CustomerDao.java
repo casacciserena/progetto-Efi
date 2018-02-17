2fadb24d10f312c5552321554a3e6b0d8303e15c
efinova <efinova@yahoo.it>
2018-02-17 02:49:45 +0100

    define structure project

--- (Struttura)/backend/src/main/java/com/Efi/beachmanager/dao/customer/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.Efi.beachmanager.dao.customer;
-
-/**
- * Created by Nova Stefano on 25/12/2017.
- */
-public interface CustomerDao {
-    public List<Customer> getAllCustomer();
-    //public void insertCategory(String category);
-}
---
+++ backend/src/main/java/com/serena/animalproject/configuration/HibernateConfiguration.java
@@ -0,0 +1,71 @@
+package it.thinkopen.configuration;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * Created by Francesco Arciello on 13/03/17.
+ */
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({"it.thinkopen"})
+@PropertySource(value = {"classpath:application.properties"})
+public class HibernateConfiguration {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(new String[]{"it.thinkopen.orm"});
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setUsername(environment.getRequiredProperty("hibernate.connection.username"));
+        dataSource.setPassword(environment.getRequiredProperty("hibernate.connection.password"));
+        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        String hbm2ddl = environment.getRequiredProperty("hibernate.hbm2ddl.auto");
+        if(hbm2ddl != null && !"".equalsIgnoreCase(hbm2ddl)) {
+            properties.put("hibernate.hbm2ddl.auto", hbm2ddl);
+        }
+        properties.put("hibernate.connection.username", environment.getRequiredProperty("hibernate.connection.username"));
+        properties.put("hibernate.connection.password", environment.getRequiredProperty("hibernate.connection.password"));
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        return properties;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory s) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(s);
+        return txManager;
+    }
+}
