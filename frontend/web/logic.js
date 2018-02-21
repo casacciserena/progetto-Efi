904fd96095192d50bf0b7b3552c62ef174066e1b
Kira Rose <casacciserena@gmail.com>
2018-02-21 23:06:56 +0100

    creazioni nuovi codici

--- frontend/web/logic.js
+++ frontend/web/logic.js
@@ -1,6 +1,17 @@
 
 function myFunction() {
 	console.log("Ciao Serena, come va oggi?");
+    var nome = document.getElementById("name").value; 
+    var arrayNome = nome.slit("");
+    if(arreyNome[arreyNome.length-] == "o"|| arreyNome[arreyNome.length-] == "O") {
+	    document.getElementById("nome").style.color "lightblue";
+   } if(arreyNome[arreyNome.length-] == "a"|| arreyNome[arreyNome.length-] == "A") {
+	document.getElementById("nome").style.color "pink"; 
+   } else {
+	document.getElementById("nome").style.color "lightgreen";
+   }		
+	document.getElementById("nome").innerHTML = " " + nome;
 	document.getElementById("fraseCss").style.display = "initial";
 }
+ 
 		
