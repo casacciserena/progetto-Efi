4cc7f6f411718a5841d391f19e57f1a4cb30b719
Kira Rose <casacciserena@gmail.com>
2018-02-21 18:58:07 +0100

    scritta serena viola scuro del bottone

--- frontend/web/logic.js
+++ frontend/web/logic.js
@@ -1,6 +1,6 @@
 
 function myFunction() {
 	console.log("Ciao Serena, come va oggi?");
-	document.getElementById("fraseCss").style.display = "visible";
+	document.getElementById("fraseCss").style.display = "initial";
 }
 		
