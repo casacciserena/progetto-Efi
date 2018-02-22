3cf90b84969c26813e59144a3067343467509e0c
Kira Rose <casacciserena@gmail.com>
2018-02-22 22:09:58 +0100

    usato swichhal posto di If

--- frontend/web/logic.js
+++ frontend/web/logic.js
@@ -9,17 +9,32 @@ function myFunction() {
     var arrayNome = nome.split("");
 	console.log(arrayNome);
 	
-    if (arrayNome[arrayNome.length-1] == "o" || arrayNome[arrayNome.length-1] == "O") {
+   /* if (arrayNome[arrayNome.length-1] == "o" || arrayNome[arrayNome.length-1] == "O") {
 		elementNome.style.color = "lightblue";
 	} else if (arrayNome[arrayNome.length-1] == "a" || arrayNome[arrayNome.length-1] == "A") {
 		elementNome.style.color = "pink"; 
 	} else {
 		elementNome.style.color = "green";
-	}		
+	}	*/
+switch	(nome) {
+	case 0: 
+	  text: "o" ; "O";
+	   color: lightblue; 
+	   block;
+	case 1:   
+	  text: "a"; "A";
+	   color: pink;
+	   block;
+	case 2:
+	   text: "else"
+	   color: green;
+	   block;
+}
 	
 	elementNome.innerHTML = " " + nome;
 	
 	document.getElementById("fraseCss").style.display = "initial";
 }
+
  
 		
