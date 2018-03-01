dcdf12d6bb30505c70d9dc147ee1990ecb952eaf
Kira Rose <casacciserena@gmail.com>
2018-03-01 19:21:36 +0100

    sistemazione grafica generale

--- frontend/web/logic.js
+++ frontend/web/logic.js
@@ -2,11 +2,11 @@
 function myFunction() {
 	console.log("Ciao Serena, come va oggi?");
 
-  var nome = document.getElementById("name").value;
+	var nome = document.getElementById("name").value;
 
 	var elementNome = document.getElementById("nome");
 
-  var arrayNome = nome.split("");
+	var arrayNome = nome.split("");
 	console.log(arrayNome);
 
 	/* if (arrayNome[arrayNome.length-1] == "o" || arrayNome[arrayNome.length-1] == "O") {
